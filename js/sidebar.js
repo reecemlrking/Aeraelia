61d19dc31712402d9ec16f2df9282ae3b985ce3a
Reece King <reeceking9922@gmail.com>
2024-12-08 22:40:55 -0700

    testing collapsable sidebar in chasaisa index.html

---
+++ js/sidebar.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "160px";
+    document.getElementById("main").style.marginLeft = "160px";
+}
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
