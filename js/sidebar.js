af114d873535bbce47a4887a8987890e37d8d4b9
Reece King <reeceking9922@gmail.com>
2024-12-08 22:58:47 -0700

    changed sidebar pos in sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,8 @@
 function openNav() {
     document.getElementById("sideNav").style.width = "160px";
-    document.getElementById("main").style.marginLeft = "160px";
+    document.getElementById("main").style.marginRight = "160px";
 }
 function closeNav(){
     document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginRight = "0";
 }
