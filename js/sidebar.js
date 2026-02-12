c8480935a704f627cc467e6b182a271214038512
Reece King <reeceking9922@gmail.com>
2026-02-12 15:37:47 -0700

    more styles tests

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
 console.log(getElementById('main').style.marginRight);
 function openNav() {
+	console.log(getElementById('main').style.marginRight);
 	document.getElementById("sideNav").style.width = "200px";
 	document.getElementById("main").style.marginRight = "200px";
 }
 function closeNav() {
+	console.log(getElementById('main').style.marginRight);
 	document.getElementById("sideNav").style.width = "0";
 	document.getElementById("main").style.marginRight = "10";
 }
