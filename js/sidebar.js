6c48873a2fd1609c9cb96bbc5d431e86a8507215
Reece King <reeceking9922@gmail.com>
2026-02-12 15:35:07 -0700

    testing styles in js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,9 @@
+console.log(getElementById('main').style.marginRight);
 function openNav() {
-    document.getElementById("sideNav").style.width = "200px";
-    document.getElementById("main").style.marginRight = "200px";
+	document.getElementById("sideNav").style.width = "200px";
+	document.getElementById("main").style.marginRight = "200px";
 }
-function closeNav(){
-    document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
+function closeNav() {
+	document.getElementById("sideNav").style.width = "0";
+	document.getElementById("main").style.marginRight = "10";
 }
