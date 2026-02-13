8b3bc7c65d8687f1259ead8fd8859a894e666901
Reece King <reeceking9922@gmail.com>
2026-02-13 10:03:07 -0700

    removed console logs in sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,8 @@
 function openNav() {
-	console.log("hello");
-	console.log(getElementById('main').style.marginRight);
 	document.getElementById("sideNav").style.width = "200px";
 	document.getElementById("main").style.marginRight = "200px";
 }
 function closeNav() {
-	console.log(getElementById('main').style.marginRight);
 	document.getElementById("sideNav").style.width = "0";
 	document.getElementById("main").style.marginRight = "10";
 }
